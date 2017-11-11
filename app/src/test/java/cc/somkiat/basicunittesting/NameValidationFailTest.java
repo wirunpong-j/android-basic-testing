468035ac57dc33a940424f7528484141112d70c7
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-11-11 23:05:46 +0700

    Refracter Case 4,5,6

--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
@@ -4,7 +4,6 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertFalse;
 
 /**
  * Created by BellKunG on 10/11/2017 AD.
@@ -36,21 +35,21 @@ public class NameValidationFailTest {
     @Test
     public void nameIsMoreThanFifteen() {
         NameValidation validation = new NameValidation();
-        boolean result = validation.validate("TachibanaMakotoa");
+        boolean result = validation.validate("Bellkungziizanaa");
         Assert.assertFalse(result);
     }
 
     @Test
     public void nameContainNumber() {
         NameValidation validation = new NameValidation();
-        boolean result = validation.validate("Bell69");
+        boolean result = validation.validate("Bellkung69");
         Assert.assertFalse(result);
     }
 
     @Test
     public void nameContainSpecialCharacter() {
         NameValidation validation = new NameValidation();
-        boolean result = validation.validate("Bell!@#$%^&{}");
+        boolean result = validation.validate("B@#ell!kun^&{}");
         Assert.assertFalse(result);
     }
 
